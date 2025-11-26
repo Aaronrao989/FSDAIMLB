141e7440039e5fcfceb8716c3e76c3083c65b559
Aaron Rao <aaronrao546@gmail.com>
2025-11-26 15:25:25 +0530

    api

--- ReactWork/shoppingcart/src/component/dashboard.jsx
+++ ReactWork/shoppingcart/src/component/dashboard.jsx
@@ -1,20 +1,60 @@
 import React, { useEffect, useState } from 'react';
 
 function Dashboard() {
-  const [counter, setCounter] = useState(10);
+  const [data, setData] = useState([]);
+  const [Cart, setCart] = useState([]);
 
   useEffect(() => {
-    console.log(counter);
-  }) // ✅ runs whenever counter changes
+    async function fetchData() {
+      const serverResponse = await fetch('https://fakestoreapi.com/products');
+      const jsonData = await serverResponse.json();
+      setData(jsonData); 
+      console.log(jsonData); 
+    }
+    fetchData();
+  }, []);
+
+  function AddToCart(ele) {
+    setCart(item => [...item, ele]);
+    alert(Cart.length);
+  }
 
   return (
     <div>
+      {
+        Cart.length === 0 ? (
+          <h2>Cart is empty</h2>
+        ) : (
+          Cart.map(ele => (
+            <div>
+              <h2>{ele.title}</h2>
+            </div>
+          ))
+        )
+      }
       <div>
-        <h2>
-          counter = {counter}
-        </h2>
+        {
+          data.length === 0 ? (
+            <h2>Data is not available</h2>
+          ) : (
+            <div>
+              {data.map(ele => (
+                <div style={{border:'2px solid green'}}>
+                  <img src={ele.image} width={100} height={100} alt={ele.title} />
+                  <h2>{ele.title}</h2>
+                  <h2>{ele.price}</h2>
+                  <h2>{ele.description}</h2>
+                  <h2>{ele.category}</h2>
+                  <button onClick={() => AddToCart(ele)}>Add to cart</button>
+                </div>
+              ))}
+            </div>
+          )
+        }
+        {
+          // JSON.stringify(data)
+        }
       </div>
-      <button onClick={() => setCounter(counter + 1)}>Increment Counter</button>
     </div>
   );
 }
