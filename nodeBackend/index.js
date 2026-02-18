07b1212aa9b885a8b7fdba42ae65d389b14d4811
Aaron Rao <aaronrao546@gmail.com>
2026-02-18 14:32:43 +0530

    backend_work

--- nodeBackend/index.js
+++ nodeBackend/index.js
@@ -1,28 +1,58 @@
-const http = require('http');
-const PORT = 4007;
+const { create } = require('domain');
+const sum=require('./fetchData');
+const http=require('http');
+const PORT=4007;
+const server=http.createServer(async(req,res)=>{
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/msg' && req.method === 'GET') {
-        res.setHeader("Content-Type", "text/html"); 
-        res.end("<h2>Hello, Welcome message from Node Server</h2>");
+    // Enable CORS
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        res.writeHead(204);
+        res.end();
+        return;
     }
 
-    if (req.url === '/data' && req.method === 'GET') {
-        res.setHeader("Content-Type", "application/json");
-        res.end(JSON.stringify({ msg: "This is json data" }));
+    // res.setHeader('Content-Type','text/html');
+    // res.end('<h2>Hello from backend server</h2>');
+    if(req.url=='/msg' && req.method=='GET'){
+        res.setHeader('Content-Type','text/html');
+        res.end('<h2>Hello message from node server</h2>');
     }
 
-    if (req.url === '/data' && req.method === 'POST') {
-        res.setHeader("Content-Type", "application/json");
-        res.end(JSON.stringify({ msg: "Post message for data insertion" }));
+
+    if(req.url=='/data' && req.method=='GET'){
+        // const data={
+        //     id:101,
+        //     name:'Amit',
+        //     course:'FSD'
+        // }
+        res.setHeader('Content-Type','application/json');
+        const sumData=await sum();
+        res.end(JSON.stringify({msg:sumData}));
     }
 
-    if (req.url === '/data' && req.method === 'DELETE') {
-        res.setHeader("Content-Type", "application/json");
-        res.end(JSON.stringify({ msg: "Resource deleted successfully" }));
+    if(req.url=='/data' && req.method=='POST'){
+         const data={
+            id:101,
+            name:'Amit',
+            course:'FSD'
+        }
+        res.setHeader('Content-Type','application/json');
+        res.end(JSON.stringify(data));
     }
-});
 
-server.listen(PORT, () => {
-    console.log(`Service is available at http://localhost:${PORT}`);
-});
+    if(req.url=='/data' && req.method=='DELETE'){
+         const data={
+            message: 'Resource deleted successfully'
+        }
+        res.setHeader('Content-Type','application/json');
+        res.end(JSON.stringify(data));
+    }
+
+})
+server.listen(PORT,()=>{
+    console.log(`Server is available on port ${PORT}`);
+})
